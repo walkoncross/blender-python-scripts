23d74d13368fee95255e898bb3fdd0bf9250dd72
zhaoyafei <zhaoyafei0210@gmail.com>
2024-09-13 13:57:36 +0300

    add scripts/hide_all_objects.py

---
+++ scripts/hide_all_objects.py
@@ -0,0 +1,18 @@
+import bpy
+
+
+def hide_all_objects() -> None:
+    """
+    Hide all objects in the current scene
+    """
+    # Ensure execution in the 3D view context
+    for area in bpy.context.screen.areas:
+        if area.type == 'VIEW_3D':
+            with bpy.context.temp_override(area=area):
+                bpy.ops.object.select_all(action='SELECT')
+                bpy.ops.object.hide_view_set(unselected=False)
+                bpy.ops.object.select_all(action='DESELECT')
+            break
+
+
+hide_all_objects()
