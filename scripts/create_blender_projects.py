e418c4f8f84d68ed9fd2f8ab28415f20e76c720b
zhaoyafei <zhaoyafei0210@gmail.com>
2024-09-15 01:00:50 +0300

    add scripts/create_blender_projects.py

---
+++ scripts/create_blender_projects.py
@@ -0,0 +1,20 @@
+import bpy
+
+# create a new default project
+# bpy.ops.preferences.addon_disable(module='ComfyUI Node Editor')
+bpy.ops.wm.read_factory_settings(use_empty=False)
+
+# save project
+file_path = "./default_blender_project.blend"
+bpy.ops.wm.save_as_mainfile(filepath=file_path)
+
+print(f"Blender project saved at: {file_path}")
+
+# create a new empty project
+bpy.ops.wm.read_factory_settings(use_empty=True)
+
+# save project
+file_path = "./empty_blender_project.blend"
+bpy.ops.wm.save_as_mainfile(filepath=file_path)
+
+print(f"Blender project saved at: {file_path}")
