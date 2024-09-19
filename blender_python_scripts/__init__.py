e3ccfe787a5e6e874d144ed9dbf25215ae8ce2ce
zhaoyafei <zhaoyafei0210@gmail.com>
2024-09-19 03:10:21 +0300

    update blender_python_scripts/__init__.py

--- blender_python_scripts/__init__.py
+++ blender_python_scripts/__init__.py
@@ -11,23 +11,12 @@ from .get_mesh_vertices_as_numpy import *
 from .get_shape_key_names import *
 from .get_uv_coords_as_numpy import *
 from .hide_all_objects import *
-from .import_assets_into_blendfile import *
 from .import_fbx import *
 from .import_gltf_or_glb import *
 from .import_obj_file import *
 from .import_obj_file_as_shape_key import *
-# from .keyframe_arkit_bs_from_livelinkface_csv_for_nv_glb import *
-# from .keyframe_arkit_bs_from_livelinkface_csv_for_polywink import *
-# from .keyframe_arkit_bs_from_livelinkface_csv_for_vroid import *
-# from .keyframe_arkit_bs_from_livelinkface_csv_for_wolf3d import *
-# from .keyframe_arkit_bs_from_nv_a2f_csv_for_nv_glb import *
-# from .keyframe_arkit_bs_from_nv_a2f_csv_for_polywink import *
-# from .keyframe_arkit_bs_from_nv_a2f_csv_for_vroid import *
-# from .keyframe_arkit_bs_from_nv_a2f_csv_for_wolf3d import *
-from .print_action_names import *
 from .rename_shape_key import *
 from .reset_armature_pose_bones import *
 from .reset_shape_key_values import *
-from .save_selected_verts import *
 from .set_minmax_range_for_shape_key import *
 from .show_all_hiden_objects import *
