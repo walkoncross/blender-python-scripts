8f62d5ac0a6b9ba212b03acbb3942aec4562c31d
zhaoyafei <zhaoyafei0210@gmail.com>
2024-10-16 02:33:23 +0300

    update bpy_wrappers/set_minmax_range_for_shape_key.py

--- bpy_wrappers/set_minmax_range_for_shape_key.py
+++ bpy_wrappers/set_minmax_range_for_shape_key.py
@@ -1,52 +1,100 @@
-"""Delete a shape key by name from an object.
+"""Set (min, max) value range for a shape key by name from an object.
 
 Author: zhaoyafei0210@gmail.com
 """
 import bpy
 
 
-def set_minmax_range_for_shape_key(bpy_obj, key_name, min=0, max=1.0):
-    """ Delete shape_key by name
+def set_minmax_range_for_shape_key(
+        bpy_obj, 
+        key_name, 
+        min=0, 
+        max=1.0
+):
+    """Set the minimum and maximum value range for a specific shape key.
+
+    Args:
+        bpy_obj (str or bpy.types.Object): The Blender object containing the shape keys.
+        key_name (str): The name of the shape key to modify.
+        min (float, optional): The minimum value for the shape key slider. Defaults to 0.
+        max (float, optional): The maximum value for the shape key slider. Defaults to 1.0.
+
+    Raises:
+        KeyError: If the specified key_name is not found in the object's shape keys.
+
+    Note:
+        This function assumes that the object has shape keys. Make sure to check
+        if bpy_obj.data.shape_keys exists before calling this function.
     """
-    print('===> set_minmax_range_for_all_shape_key(): ')
-    print('---> mesh: {}'.format(bpy_obj.name))
-    print('---> min: {}'.format(min))
-    print('---> max: {}'.format(max))
+    if isinstance(bpy_obj, str):
+        bpy_obj = bpy.data.objects[bpy_obj]
 
-    # bpy_obj = bpy.data.objects[mesh_obj_name]
+    print(f'===> set_minmax_range_for_shape_key(): ')
+    print(f'---> mesh: {bpy_obj.name}')
+    print(f'---> key: {key_name}')
+    print(f'---> min: {min}')
+    print(f'---> max: {max}')
 
-    # setting the active shapekey
-    index = bpy_obj.data.shape_keys.key_blocks.keys().index(key_name)
-    bpy_obj.active_shape_key_index = index
+    if key_name not in bpy_obj.data.shape_keys.key_blocks:
+        raise KeyError(f"Shape key '{key_name}' not found in object '{bpy_obj.name}'")
 
-    bpy_obj.data.shape_keys.key_blocks[key_name].slider_min = min
-    bpy_obj.data.shape_keys.key_blocks[key_name].slider_max = max
+    # Set the active shape key
+    bpy_obj.active_shape_key_index = bpy_obj.data.shape_keys.key_blocks.find(key_name)
+
+    # Set the min and max values
+    shape_key = bpy_obj.data.shape_keys.key_blocks[key_name]
+    shape_key.slider_min = min
+    shape_key.slider_max = max
 
 
-def set_minmax_range_for_all_shape_key(bpy_obj, min=0, max=1.0):
-    """ Delete shape_key by name
+def set_minmax_range_for_all_shape_key(
+        bpy_obj,
+        min=0, 
+        max=1.0
+):
+    """Set the minimum and maximum value range for all shape keys of a Blender object.
+
+    This function iterates through all shape keys of the given object and sets
+    their minimum and maximum slider values to the specified values.
+
+    Args:
+        bpy_obj (str or bpy.types.Object): The Blender object containing the shape keys.
+        min (float, optional): The minimum value for all shape key sliders. Defaults to 0.
+        max (float, optional): The maximum value for all shape key sliders. Defaults to 1.0.
+
+    Note:
+        This function assumes that the object has shape keys. Make sure to check
+        if bpy_obj.data.shape_keys exists before calling this function.
+
+    Example:
+        >>> obj = bpy.context.active_object
+        >>> set_minmax_range_for_all_shape_key(obj, min=-1.0, max=1.0)
     """
-    print('===> set_minmax_range_for_all_shape_key(): ')
-    print('---> mesh: {}'.format(bpy_obj.name))
-    print('---> min: {}'.format(min))
-    print('---> max: {}'.format(max))
 
-    # bpy_obj = bpy.data.objects[mesh_obj_name]
+    if isinstance(bpy_obj, str):
+        bpy_obj = bpy.data.objects[bpy_obj]
 
-    # setting the active shapekey
-    for ind, kb in enumerate(bpy_obj.data.shape_keys.key_blocks):
-        bpy_obj.active_shape_key_index = ind
-        key_name = kb.name
+    print(f'===> set_minmax_range_for_all_shape_key(): ')
+    print(f'---> mesh: {bpy_obj.name}')
+    print(f'---> min: {min}')
+    print(f'---> max: {max}')
 
-        bpy_obj.data.shape_keys.key_blocks[key_name].slider_min = min
-        bpy_obj.data.shape_keys.key_blocks[key_name].slider_max = max
+    if not bpy_obj.data.shape_keys:
+        print(f"Warning: Object '{bpy_obj.name}' has no shape keys.")
+        return
 
+    # Iterate through all shape keys and set their min/max values
+    for shape_key in bpy_obj.data.shape_keys.key_blocks:
+        shape_key.slider_min = min
+        shape_key.slider_max = max
+
+    print(f"Set min/max range for all shape keys of '{bpy_obj.name}' to [{min}, {max}]")
 
 if __name__ == '__main__':
     bpy_obj = bpy.context.active_object
     min_val = -1.0
     max_val = 1.0
 
-    key_name = 'jawOpen'
-    set_minmax_range_for_shape_key(bpy_obj, key_name, min_val, max_val)
-    # set_minmax_range_for_all_shape_key(bpy_obj, min_val, max_val)
+    # key_name = 'JawOpen'
+    # set_minmax_range_for_shape_key(bpy_obj, key_name, min_val, max_val)
+    set_minmax_range_for_all_shape_key(bpy_obj, min_val, max_val)
